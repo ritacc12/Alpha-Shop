f5ac7b918657be21505c98ea6ba887d6e22669fb
ritacc12 <rita20920@gmail.com>
2023-12-14 17:17:29 +0800

    add the className of input & set up the form layout

--- src/component/form/Step3.jsx
+++ src/component/form/Step3.jsx
@@ -1,3 +1,5 @@
+import "../../Styles/Step3.scss";
+
 export default function Step3() {
   return (
     <>
@@ -6,19 +8,19 @@ export default function Step3() {
           <h3 className="form-title">付款資訊</h3>
           <section className="form-body col col-12">
             <div className="input-group">
-              <div className="input-label">持卡人姓名</div>
+              <div className="input-label name">持卡人姓名</div>
               <input className="name" type="text" placeholder="John Doe" />
             </div>
             <div className="input-group">
-              <div className="input-label">卡號</div>
+              <div className="input-label cardNumber">卡號</div>
               <input type="text" placeholder="1111 2222 3333 4444" />
             </div>
             <div className="input-group">
-              <div className="input-label">有效期限</div>
+              <div className="input-label period">有效期限</div>
               <input type="date" />
             </div>
             <div className="input-group">
-              <div className="input-label">CVC/CCV</div>
+              <div className="input-label private">CVC/CCV</div>
               <input type="number" placeholder="123" />
             </div>
           </section>

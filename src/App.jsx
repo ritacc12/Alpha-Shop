3ca295aa19adde78c883c979adedb1bd908acf11
ritacc12 <rita20920@gmail.com>
2023-12-14 17:23:55 +0800

    import Header, Form and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import Header from "./component/Header";
+import Form from "./component/form/Form";
+import Footer from "./component/Footer";
+import "../src/Styles/Form.scss";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header></Header>
+      <div>
+        <Form></Form>
+      </div>
+      <Footer></Footer>
+    </>
+  );
 }
 
 export default App;

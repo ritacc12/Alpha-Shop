ebd9775e2d9df9aa83035ef8875e6296dc8c7ebb
ritacc12 <rita20920@gmail.com>
2023-12-18 18:31:03 +0800

    import Cart component

--- src/component/form/Form.jsx
+++ src/component/form/Form.jsx
@@ -5,6 +5,7 @@ import StepProgress from "./StepProgress";
 import Step1 from "./Step1";
 import Step2 from "./Step2";
 import Step3 from "./Step3";
+import Cart from "../Cart";
 import { useState } from "react";
 import "../../Styles/Form.scss";
 
@@ -29,6 +30,9 @@ function Form() {
             <StepProgress stage={stage} />
             {FormStage()}
             <ProgressControl stage={stage} setStage={setStage} />
+          </div>
+          <div className="form-right">
+            <Cart></Cart>
           </div>
         </div>
       </section>
